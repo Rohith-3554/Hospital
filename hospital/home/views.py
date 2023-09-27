b432c505f47a2bdedeaeb136897d86a0b3b313c9
Rohith-3554 <rohithramesh1996@gmail.com>
2023-09-27 16:58:50 +0530

    added

---
+++ hospital/home/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render,redirect
+from .models import Departments,Doctors
+from .forms import BookingForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def booking(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'confirmation.html')
+    form = BookingForm()
+    dict_form = {
+        'form': form
+    }
+    return render(request, 'booking.html', dict_form)
+
+def doctors(request):
+    dict_docs = {
+        'doctors': Doctors.objects.all()
+    }
+    return render(request, 'doctors.html', dict_docs)
+
+def contact(request):
+    return render(request,'contact.html')
+
+def login_view(request):
+    if (request.method == "POST"):
+        u = request.POST['u']
+        p = request.POST['p']
+        user=authenticate(username=u,password=p)
+        if user:
+            login(request,user)
+            return index(request)
+        else:
+            messages.error(request, "invalid user")
+    return render(request,'login.html')
+
+def register(request):
+    if(request.method=="POST"):
+        u =request.POST['u']
+        f = request.POST['f']
+        l = request.POST['l']
+        e = request.POST['e']
+        p = request.POST['p']
+        cp = request.POST['cp']
+        if(p==cp):
+            u=User.objects.create_user(username=u,password=p,email=e,first_name=f,last_name=l)
+            u.save()
+            return index(request)
+        else:
+            messages.error(request,"PASSWORDS ARE NOT SAME")
+
+    return render(request,'register.html')
+
+def logout_view(request):
+    logout(request)
+    return index(request)
+
+
+def department(request):
+    dict_dept = {
+        'dept': Departments.objects.all()
+    }
+    return render(request, 'department.html', dict_dept)
+
+
